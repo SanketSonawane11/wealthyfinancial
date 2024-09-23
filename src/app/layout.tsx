3210d8a66e4beadbe91f560fe86ac54948e6b18c
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-09-23 20:17:42 +0530

    App made

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Wealthy Financial",
+  description: "Serving with love",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
