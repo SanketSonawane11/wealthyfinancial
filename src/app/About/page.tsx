1f21ff831c0d50c87d4740d5be0a7c013e5a1709
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-09-28 10:14:07 +0530

    Fixed About us

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -2,54 +2,54 @@ import React from "react";
 import { Navbar } from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import AboutHero from "../Components/About-Hero";
-import Timeline from "../Components/Timeline";
+// import Timeline from "../Components/Timeline";
 import OurTeam from "../Components/Team";
 import ToolsAndIdeasSection from "../Components/AboutCta";
 import BusinessSection from "../Components/Mission";
 
-const timelineData = [
-  {
-    title: "Project Kickoff",
-    content: (
-      <p>
-        We held our initial project meeting to discuss the scope and
-        deliverables.
-      </p>
-    ),
-  },
-  {
-    title: "Phase 1 Completion",
-    content: (
-      <p>
-        Phase 1, focused on research and planning, was successfully completed.
-      </p>
-    ),
-  },
-  {
-    title: "Prototype Development",
-    content: (
-      <p>
-        We developed a functional prototype of the system to test the core
-        features.
-      </p>
-    ),
-  },
-  {
-    title: "User Testing",
-    content: (
-      <p>
-        Completed user testing with positive feedback. Adjustments were made
-        based on user suggestions.
-      </p>
-    ),
-  },
-  {
-    title: "Final Launch",
-    content: (
-      <p>The product was officially launched and is now live for all users.</p>
-    ),
-  },
-];
+// const timelineData = [
+//   {
+//     title: "Project Kickoff",
+//     content: (
+//       <p>
+//         We held our initial project meeting to discuss the scope and
+//         deliverables.
+//       </p>
+//     ),
+//   },
+//   {
+//     title: "Phase 1 Completion",
+//     content: (
+//       <p>
+//         Phase 1, focused on research and planning, was successfully completed.
+//       </p>
+//     ),
+//   },
+//   {
+//     title: "Prototype Development",
+//     content: (
+//       <p>
+//         We developed a functional prototype of the system to test the core
+//         features.
+//       </p>
+//     ),
+//   },
+//   {
+//     title: "User Testing",
+//     content: (
+//       <p>
+//         Completed user testing with positive feedback. Adjustments were made
+//         based on user suggestions.
+//       </p>
+//     ),
+//   },
+//   {
+//     title: "Final Launch",
+//     content: (
+//       <p>The product was officially launched and is now live for all users.</p>
+//     ),
+//   },
+// ];
 
 export default function Page() {
   return (
