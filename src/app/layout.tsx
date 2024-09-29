c9f31bc8a251b7a0d1fd6cd6984119448f538990
SanketSonawane11 <sanketsonawane424@gmail.com>
2024-09-29 14:51:20 +0530

    Added Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "./Components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Wealthy Financial",
@@ -21,6 +22,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <SpeedInsights />
           {children}
         </ThemeProvider>
       </body>
